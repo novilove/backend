0ec41ba11d9dfdbf1e85ad9a4670cfc37bf92baf
cony ross <jardlml1209@gmail.com>
2019-12-12 13:10:25 -0300

    Profe y estudiante

--- simulacro/src/main/java/com/simulacro/model/Estudiante.java
+++ simulacro/src/main/java/com/simulacro/model/Estudiante.java
@@ -1,13 +1,22 @@
 package com.simulacro.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name="Estudiante")
+@Table(name="ESTUDIANTE")
 public class Estudiante {
     @Id
+    @Column(name="ID_ESTUDIANTE")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEstudiante;
 
-    private Long id;
+    @Column(name="NOMBRE_EST", nullable = false)
+    private String nombreEst;
+    @Column(name="APELLIDO_EST", nullable = false)
+    private String apellidoEst;
+    @Column(name="RUT", unique = true, nullable = false)
+    private String rut;
+    private String correo;
+    private String contrasena;
+
 }
--- simulacro/src/main/java/com/simulacro/model/Profesor.java
+++ simulacro/src/main/java/com/simulacro/model/Profesor.java
@@ -1,11 +1,69 @@
 package com.simulacro.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name="Profesor")
+@Table(name="PROFESOR")
 public class Profesor {
+    @Id
+    @Column(name="ID_PROFESOR" )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProfesor;
 
+    @Column(name="NOMBRE_PROF", nullable = false)
+    private String nombreProf;
+    @Column(name="APELLIDO_PROF", nullable = false)
+    private String apellidoProf;
+    @Column(name="RUT", unique = true ,nullable = false)
+    private String rut;
+    @Column(name="CORREO", unique = true, nullable = false)
+    private String correo;
 
+    public Profesor(Long idProfesor, String nombreProf, String apellidoProf, String rut, String correo) {
+        this.idProfesor = idProfesor;
+        this.nombreProf = nombreProf;
+        this.apellidoProf = apellidoProf;
+        this.rut = rut;
+        this.correo = correo;
+    }
+
+    public Long getIdProfesor() {
+        return idProfesor;
+    }
+
+    public void setIdProfesor(Long idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    public String getNombreProf() {
+        return nombreProf;
+    }
+
+    public void setNombreProf(String nombreProf) {
+        this.nombreProf = nombreProf;
+    }
+
+    public String getApellidoProf() {
+        return apellidoProf;
+    }
+
+    public void setApellidoProf(String apellidoProf) {
+        this.apellidoProf = apellidoProf;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
