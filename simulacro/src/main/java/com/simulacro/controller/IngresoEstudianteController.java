063c50044abea2823ff19f04ce8ee18095b6d264
joseCris <elsolbrillaenlaluna@gmail.com>
2019-12-12 21:12:12 -0300

    porfa

--- simulacro/src/main/java/com/simulacro/controller/IngresoEstudianteController.java
+++ simulacro/src/main/java/com/simulacro/controller/IngresoEstudianteController.java
@@ -1,8 +1,10 @@
 package com.simulacro.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/ingreso_estudiantes/")
 public class IngresoEstudianteController {
 
 }
