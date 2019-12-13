6a1ca9aca0c6c1e775dc18e01b3158148b5c61fe
joseCris <elsolbrillaenlaluna@gmail.com>
2019-12-12 22:28:00 -0300

    MM

--- simulacro/src/main/java/com/simulacro/controller/IngresoEstudianteController.java
+++ simulacro/src/main/java/com/simulacro/controller/IngresoEstudianteController.java
@@ -1,10 +1,33 @@
 package com.simulacro.controller;
 
+import com.simulacro.imp.IngresoEstudiantesImplements;
+
+import com.simulacro.model.Ingreso_Estudiantes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/ingreso_estudiantes/")
+@RequestMapping("/api/v1/ingreso_estudiantes")
 public class IngresoEstudianteController {
+
+    @Autowired
+    private IngresoEstudiantesImplements estudianteImp;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Ingreso_Estudiantes guardarEstudiante(@RequestBody Ingreso_Estudiantes estudiante){
+
+        Ingreso_Estudiantes estudianteController = null;
+
+        try{
+            estudianteController = estudianteImp.guardarEstudiante(estudiante);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return estudianteController;
+    }
+
 
 }
--- simulacro/src/main/java/com/simulacro/model/Estudiantes.java
+++ simulacro/src/main/java/com/simulacro/model/Estudiantes.java
@@ -1,8 +1,8 @@
 package com.simulacro.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+        import javax.persistence.Entity;
+        import javax.persistence.Id;
+        import javax.persistence.Table;
 
 @Entity
 @Table(name="Estudiante")
